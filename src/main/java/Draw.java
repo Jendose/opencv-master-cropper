f6dd4797932cdd178c7bac57dfcaabf356826023
Jendose <zhenya.shatskov@yandex.ru>
2020-12-08 04:17:51 +0400

    add functional base for correct polygons drawing

--- src/main/java/Draw.java
+++ src/main/java/Draw.java
@@ -5,75 +5,145 @@ import org.opencv.imgproc.Imgproc;
 
 public class Draw {
 
-    public static void line(Mat image, Point point1, Point point2, Scalar color, int thickness){
-        Imgproc.line(image, point1, point2, color, thickness);
+    public static Mat line(Mat image, Point point1, Point point2, Scalar color, int thickness){
+        Mat resImage = image.clone();
+        Imgproc.line(resImage, point1, point2, color, thickness);
+        return resImage;
     }
 
-    public static void contour(Mat image, Quadrangle quadrangle, Scalar color, int thickness){
+    public static Mat contour(Mat image, Quadrangle quadrangle, Scalar color, int thickness){
+        Mat resImage = image.clone();
+
         Point topLeft = quadrangle.getTopLeft();
         Point topRight = quadrangle.getTopRight();
         Point bottomRight = quadrangle.getBottomRight();
         Point bottomLeft = quadrangle.getBottomLeft();
 
-        Imgproc.line(image, topLeft, topRight, color, thickness);
-        Imgproc.line(image, topRight, bottomRight, color, thickness);
-        Imgproc.line(image, bottomRight, bottomLeft, color, thickness);
-        Imgproc.line(image, bottomLeft, topLeft, color, thickness);
+        Imgproc.line(resImage, topLeft, topRight, color, thickness);
+        Imgproc.line(resImage, topRight, bottomRight, color, thickness);
+        Imgproc.line(resImage, bottomRight, bottomLeft, color, thickness);
+        Imgproc.line(resImage, bottomLeft, topLeft, color, thickness);
+
+        return resImage;
     }
 
-    public static void polygons(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, Scalar color, int thickness, boolean noContour){
+    // polygons строят секту прямоугольного объекта по законам перспективы
+
+    public static Mat polygons(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, Scalar color, int thickness, boolean noContour){
+        Mat resImage = image.clone();
+
         if(!noContour){
-            contour(image, quadrangle, color, thickness);
+            contour(resImage, quadrangle, color, thickness);
         }
-        int verticalLines = verticalNumber < 1? 0 : verticalNumber-1;
-        int horizontalLines = horizontalNumber < 1? 0 : horizontalNumber-1;
 
-        for (int i = 1; i<=verticalLines; i++){
-            double l = (double)i/(verticalLines-i+1);
-            Imgproc.line(image,
-                    new Point(
-                            (quadrangle.getBottomLeft().x+(quadrangle.getTopLeft().x*l))/(1+l),
-                            (quadrangle.getBottomLeft().y+(quadrangle.getTopLeft().y*l))/(1+l)),
-                    new Point(
-                            (quadrangle.getBottomRight().x+(quadrangle.getTopRight().x*l))/(1+l),
-                            (quadrangle.getBottomRight().y+(quadrangle.getTopRight().y*l))/(1+l)),
-                    color, thickness);
-        }
-        for (int i = 1; i<=horizontalLines; i++){
-            double l = (double)i/(horizontalLines-i+1);
-            Imgproc.line(image,
-                    new Point(
-                            (quadrangle.getTopLeft().x+(quadrangle.getTopRight().x*l))/(1+l),
-                            (quadrangle.getTopLeft().y+(quadrangle.getTopRight().y*l))/(1+l)),
-                    new Point(
-                            (quadrangle.getBottomLeft().x+(quadrangle.getBottomRight().x*l))/(1+l),
-                            (quadrangle.getBottomLeft().y+(quadrangle.getBottomRight().y*l))/(1+l)),
-                    color, thickness);
-        }
+        Point A = quadrangle.getTopLeft();
+        Point B = quadrangle.getTopRight();
+        Point C = quadrangle.getBottomRight();
+        Point D = quadrangle.getBottomLeft();
+
+        // Деление объекта на сегменты по вертикали происходит из точки пересечения двух прямых:
+        // 1) Проходящей через точки A, B
+        // 2) Проходящей через точки D, C
+
+        // Угловой коэффициент AB
+        double k_AB = (B.y-A.y)/(B.x-A.x);
+        // Свободный член
+        double b_AB = A.y-((A.x*(B.y-A.y))/(B.x-A.x));
+
+        // Угловой коэффициент DC
+        double k_DC = (C.y-D.y)/(C.x-D.x);
+        // Свободный член
+        double b_DC = D.y-((D.x*(C.y-D.y))/(C.x-D.x));
+
+        // Точка пересечения AB и DC
+        double point_AB_DC_x = (b_AB-b_DC)/(k_DC-k_AB);
+        Point point_AB_DC = new Point(point_AB_DC_x,k_AB*point_AB_DC_x+b_AB);
+
+        // Тангенс угла между AB и DC
+        double tg_AB_DC = (k_DC-k_AB)/(1+k_AB*k_DC);
+
+        // Угол между AB и DC - используется для вычисления угла, равного 1/verticalNumber части данного (шага по вертикали)
+        double angle_AB_DC = Math.atan(tg_AB_DC);
+
+        // Деление объекта на сегменты по горизонтали происходит из точки пересечения двух прямых:
+        // 1) Проходящей через точки A, D
+        // 2) Проходящей через точки B, C
+
+        // Угловой коэффициент AD
+        double k_AD = (D.y-A.y)/(D.x-A.x);
+        // Свободный член
+        double b_AD = A.y-((A.x*(D.y-A.y))/(D.x-A.x));
+
+        // Угловой коэффициент BC
+        double k_BC = (C.y-B.y)/(C.x-B.x);
+        // Свободный член
+        double b_BC = B.y-((B.x*(C.y-B.y))/(C.x-B.x));
+
+        // Точка пересечения AD и BC
+        double point_AD_BC_x = (b_AD-b_BC)/(k_BC-k_AD);
+        Point point_AD_BC = new Point(point_AD_BC_x,k_AD*point_AD_BC_x+b_AD);
+
+        // Тангенс угла между AD и BC
+        double tg_AD_BC = (k_BC-k_AD)/(1+k_AD*k_BC);
+
+        // Угол между AD и BC - используется для вычисления угла, равного 1/horizontalNumber части данного (шага по горизонтали)
+        double angle_AD_BC = Math.atan(tg_AD_BC);
+
+        int widthLines = verticalNumber < 1? 0 : verticalNumber-1;
+        int heightLines = horizontalNumber < 1? 0 : horizontalNumber-1;
+
+//        for (int i = 1; i<=widthLines; i++){
+//            double l = (double)i/(widthLines-i+1);
+//            Imgproc.line(resImage,
+//                    new Point(
+//                            (quadrangle.getBottomLeft().x+(quadrangle.getTopLeft().x*l))/(1+l),
+//                            (quadrangle.getBottomLeft().y+(quadrangle.getTopLeft().y*l))/(1+l)),
+//                    new Point(
+//                            (quadrangle.getBottomRight().x+(quadrangle.getTopRight().x*l))/(1+l),
+//                            (quadrangle.getBottomRight().y+(quadrangle.getTopRight().y*l))/(1+l)),
+//                    color, thickness);
+//        }
+//        for (int i = 1; i<=heightLines; i++){
+//            double l = (double)i/(heightLines-i+1);
+//            Imgproc.line(resImage,
+//                    new Point(
+//                            (quadrangle.getTopLeft().x+(quadrangle.getTopRight().x*l))/(1+l),
+//                            (quadrangle.getTopLeft().y+(quadrangle.getTopRight().y*l))/(1+l)),
+//                    new Point(
+//                            (quadrangle.getBottomLeft().x+(quadrangle.getBottomRight().x*l))/(1+l),
+//                            (quadrangle.getBottomLeft().y+(quadrangle.getBottomRight().y*l))/(1+l)),
+//                    color, thickness);
+//        }
+
+        return resImage;
     }
 
-    public static void polygons(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, Scalar color, int thickness){
-        polygons(image, quadrangle, verticalNumber, horizontalNumber, color, thickness, false);
+    public static Mat polygons(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, Scalar color, int thickness){
+        return polygons(image, quadrangle, verticalNumber, horizontalNumber, color, thickness, false);
     }
 
-    public static void polygons(Mat image, Quadrangle quadrangle, int number, Scalar color, int thickness, boolean noContour){
-        polygons(image, quadrangle, number, number, color, thickness, noContour);
+    public static Mat polygons(Mat image, Quadrangle quadrangle, int number, Scalar color, int thickness, boolean noContour){
+        return polygons(image, quadrangle, number, number, color, thickness, noContour);
     }
 
-    public static void polygons(Mat image, Quadrangle quadrangle, int number, Scalar color, int thickness){
-        polygons(image, quadrangle, number, number, color, thickness, false);
+    public static Mat polygons(Mat image, Quadrangle quadrangle, int number, Scalar color, int thickness){
+        return polygons(image, quadrangle, number, number, color, thickness, false);
     }
 
-    public static void polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, int crossThickness, boolean noContour){
+    // polygonsCross позволяют выделить пересечение двух линий на сетке polygons
+
+    public static Mat polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, int crossThickness, boolean noContour){
+        Mat resImage = image.clone();
+
         if(!noContour){
-            contour(image, quadrangle, mainColor, mainThickness);
+            contour(resImage, quadrangle, mainColor, mainThickness);
         }
-        int verticalLines = verticalNumber < 1? 0 : verticalNumber-1;
-        int horizontalLines = horizontalNumber < 1? 0 : horizontalNumber-1;
+        int widthLines = verticalNumber < 1? 0 : verticalNumber-1;
+        int heightLines = horizontalNumber < 1? 0 : horizontalNumber-1;
 
-        for (int i = 1; i<=verticalLines; i++){
-            double l = (double)i/(verticalLines-i+1);
-            Imgproc.line(image,
+        for (int i = 1; i<=widthLines; i++){
+            double l = (double)i/(widthLines-i+1);
+            Imgproc.line(resImage,
                     new Point(
                             (quadrangle.getBottomLeft().x+(quadrangle.getTopLeft().x*l))/(1+l),
                             (quadrangle.getBottomLeft().y+(quadrangle.getTopLeft().y*l))/(1+l)),
@@ -82,9 +152,9 @@ public class Draw {
                             (quadrangle.getBottomRight().y+(quadrangle.getTopRight().y*l))/(1+l)),
                     i==verticalOrdinal?crossColor:mainColor, i==verticalOrdinal?crossThickness:mainThickness);
         }
-        for (int i = 1; i<=horizontalLines; i++){
-            double l = (double)i/(horizontalLines-i+1);
-            Imgproc.line(image,
+        for (int i = 1; i<=heightLines; i++){
+            double l = (double)i/(heightLines-i+1);
+            Imgproc.line(resImage,
                     new Point(
                             (quadrangle.getTopLeft().x+(quadrangle.getTopRight().x*l))/(1+l),
                             (quadrangle.getTopLeft().y+(quadrangle.getTopRight().y*l))/(1+l)),
@@ -93,18 +163,121 @@ public class Draw {
                             (quadrangle.getBottomLeft().y+(quadrangle.getBottomRight().y*l))/(1+l)),
                     i==horizontalOrdinal?crossColor:mainColor, i==horizontalOrdinal?crossThickness:mainThickness);
         }
+
+        return resImage;
     }
 
-    public static void polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, int crossThickness){
-        polygonsCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, crossThickness, false);
+    public static Mat polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, int crossThickness){
+        return polygonsCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, crossThickness, false);
     }
 
-    public static void polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, boolean noContour){
-        polygonsCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, mainThickness*3, noContour);
+    public static Mat polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, boolean noContour){
+        return polygonsCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, mainThickness*3, noContour);
     }
 
-    public static void polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness){
-        polygonsCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, mainThickness*3, false);
+    public static Mat polygonsCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness){
+        return polygonsCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, mainThickness*3, false);
+    }
+
+
+
+    //----------------------------------------------------------------------------------------------------------------//
+    // polygonsFlat строят сетку, разделяя стороны четырехугольника на отрезки
+
+    public static Mat polygonsFlat(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, Scalar color, int thickness, boolean noContour){
+        Mat resImage = image.clone();
+
+        if(!noContour){
+            contour(resImage, quadrangle, color, thickness);
+        }
+        int widthLines = verticalNumber < 1? 0 : verticalNumber-1;
+        int heightLines = horizontalNumber < 1? 0 : horizontalNumber-1;
+
+        for (int i = 1; i<=widthLines; i++){
+            double l = (double)i/(widthLines-i+1);
+            Imgproc.line(resImage,
+                    new Point(
+                            (quadrangle.getBottomLeft().x+(quadrangle.getTopLeft().x*l))/(1+l),
+                            (quadrangle.getBottomLeft().y+(quadrangle.getTopLeft().y*l))/(1+l)),
+                    new Point(
+                            (quadrangle.getBottomRight().x+(quadrangle.getTopRight().x*l))/(1+l),
+                            (quadrangle.getBottomRight().y+(quadrangle.getTopRight().y*l))/(1+l)),
+                    color, thickness);
+        }
+        for (int i = 1; i<=heightLines; i++){
+            double l = (double)i/(heightLines-i+1);
+            Imgproc.line(resImage,
+                    new Point(
+                            (quadrangle.getTopLeft().x+(quadrangle.getTopRight().x*l))/(1+l),
+                            (quadrangle.getTopLeft().y+(quadrangle.getTopRight().y*l))/(1+l)),
+                    new Point(
+                            (quadrangle.getBottomLeft().x+(quadrangle.getBottomRight().x*l))/(1+l),
+                            (quadrangle.getBottomLeft().y+(quadrangle.getBottomRight().y*l))/(1+l)),
+                    color, thickness);
+        }
+
+        return resImage;
+    }
+
+    public static Mat polygonsFlat(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, Scalar color, int thickness){
+        return polygonsFlat(image, quadrangle, verticalNumber, horizontalNumber, color, thickness, false);
+    }
+
+    public static Mat polygonsFlat(Mat image, Quadrangle quadrangle, int number, Scalar color, int thickness, boolean noContour){
+        return polygonsFlat(image, quadrangle, number, number, color, thickness, noContour);
+    }
+
+    public static Mat polygonsFlat(Mat image, Quadrangle quadrangle, int number, Scalar color, int thickness){
+        return polygonsFlat(image, quadrangle, number, number, color, thickness, false);
+    }
+
+    // polygonsFlatCross позволяют выделить пересечение двух линий на сетке polygonsFlat
+
+    public static Mat polygonsFlatCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, int crossThickness, boolean noContour){
+        Mat resImage = image.clone();
+
+        if(!noContour){
+            contour(resImage, quadrangle, mainColor, mainThickness);
+        }
+        int widthLines = verticalNumber < 1? 0 : verticalNumber-1;
+        int heightLines = horizontalNumber < 1? 0 : horizontalNumber-1;
+
+        for (int i = 1; i<=widthLines; i++){
+            double l = (double)i/(widthLines-i+1);
+            Imgproc.line(resImage,
+                    new Point(
+                            (quadrangle.getBottomLeft().x+(quadrangle.getTopLeft().x*l))/(1+l),
+                            (quadrangle.getBottomLeft().y+(quadrangle.getTopLeft().y*l))/(1+l)),
+                    new Point(
+                            (quadrangle.getBottomRight().x+(quadrangle.getTopRight().x*l))/(1+l),
+                            (quadrangle.getBottomRight().y+(quadrangle.getTopRight().y*l))/(1+l)),
+                    i==verticalOrdinal?crossColor:mainColor, i==verticalOrdinal?crossThickness:mainThickness);
+        }
+        for (int i = 1; i<=heightLines; i++){
+            double l = (double)i/(heightLines-i+1);
+            Imgproc.line(resImage,
+                    new Point(
+                            (quadrangle.getTopLeft().x+(quadrangle.getTopRight().x*l))/(1+l),
+                            (quadrangle.getTopLeft().y+(quadrangle.getTopRight().y*l))/(1+l)),
+                    new Point(
+                            (quadrangle.getBottomLeft().x+(quadrangle.getBottomRight().x*l))/(1+l),
+                            (quadrangle.getBottomLeft().y+(quadrangle.getBottomRight().y*l))/(1+l)),
+                    i==horizontalOrdinal?crossColor:mainColor, i==horizontalOrdinal?crossThickness:mainThickness);
+        }
+
+        return resImage;
+    }
+
+    public static Mat polygonsFlatCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, int crossThickness){
+        return polygonsFlatCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, crossThickness, false);
+    }
+
+    public static Mat polygonsFlatCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness, boolean noContour){
+        return polygonsFlatCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, mainThickness*3, noContour);
+    }
+
+    public static Mat polygonsFlatCross(Mat image, Quadrangle quadrangle, int verticalNumber, int horizontalNumber, int verticalOrdinal, int horizontalOrdinal, Scalar mainColor, Scalar crossColor, int mainThickness){
+        return polygonsFlatCross(image, quadrangle, verticalNumber, horizontalNumber, verticalOrdinal, horizontalOrdinal, mainColor, crossColor, mainThickness, mainThickness*3, false);
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,6 @@
 import nu.pattern.OpenCV;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.objdetect.Objdetect;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
@@ -24,31 +16,86 @@ public class Main {
         Imgcodecs.imwrite(targetPath, imageMatrix);
     }
 
-    public static void saveVariant(Mat input, Quadrangle testQuadrangle, int horizontalOrdinal, int verticalOrdinal, int testNumber){
-        Mat resImage = input.clone();
-        Draw.contour(resImage, testQuadrangle, new Scalar(255,0,77), 5);
-        Draw.polygonsCross(resImage, testQuadrangle, 20, 42, verticalOrdinal, horizontalOrdinal, new Scalar(255,255,255), new Scalar(0,255,255), 5);
-        saveImage(resImage, "./src/main/resources/img/test_opencv_poligons-20-42+"+ testNumber +".jpg");
+    public static void saveFlatCross(Mat image, Quadrangle testQuadrangle, int verticalOrdinal, int horizontalOrdinal, String path){
+        Mat resImage = image.clone();
+        // Color: BRG
+        resImage = Draw.polygonsFlatCross(resImage, testQuadrangle, 20, 42, verticalOrdinal, horizontalOrdinal, new Scalar(255,255,255), new Scalar(0,255,255), 5);
+        saveImage(resImage, path);
+    }
+
+    public static void saveCross(Mat image, Quadrangle testQuadrangle, int horizontalOrdinal, int verticalOrdinal, String path){
+        Mat resImage = image.clone();
+        // Color: BRG
+        resImage = Draw.polygonsCross(resImage, testQuadrangle, 20, 42, verticalOrdinal, horizontalOrdinal, new Scalar(255,255,255), new Scalar(0,255,255), 5);
+        saveImage(resImage, path);
     }
 
     public static void main(String[] args) {
 
-        Quadrangle testQuadrangle = new Quadrangle(
+        Quadrangle verticalObject = new Quadrangle(
                 new Point(378, 490),
                 new Point(3672, 1405),
                 new Point(3154, 2770),
                 new Point(783, 1602)
         );
 
-        Mat loadedImage = loadImage("./src/main/resources/img/test.jpg");
-        Mat newImage = loadedImage.clone();
-                                                       // brg
-        Draw.contour(newImage, testQuadrangle, new Scalar(255,0,77), 5);
-//        Draw.polygons(newImage, testQuadrangle, 20, 42, new Scalar(255,255,255), 5, true);
+        Mat verticalObjectImageSource = loadImage("./src/main/resources/img/examples/vertical/vertical.jpg");
+        Mat verticalObjectImage = verticalObjectImageSource.clone();
 
-        saveVariant(newImage, testQuadrangle, 21, 10,0);
-        saveVariant(newImage, testQuadrangle, 20, 10,1);
-        saveVariant(newImage, testQuadrangle, 19, 9,2);
-        saveVariant(newImage, testQuadrangle, 18, 9,3);
+        Quadrangle horizontalObject = new Quadrangle(
+                new Point(482, 1517),
+                new Point(2606, 546),
+                new Point(3581, 885),
+                new Point(1335, 2542)
+        );
+
+        Mat horizontalObjectImageSource = loadImage("./src/main/resources/img/examples/horizontal/horizontal.jpg");
+        Mat horizontalObjectImage = horizontalObjectImageSource.clone();
+
+
+
+        // Vertical flat
+        saveFlatCross(verticalObjectImage, verticalObject, 10, 21,"./src/main/resources/img/examples/vertical/vertical_polygonsFlat-20-42_cross-3.jpg");
+        saveFlatCross(verticalObjectImage, verticalObject, 10, 20,"./src/main/resources/img/examples/vertical/vertical_polygonsFlat-20-42_cross-2.jpg");
+        saveFlatCross(verticalObjectImage, verticalObject, 9, 19,"./src/main/resources/img/examples/vertical/vertical_polygonsFlat-20-42_cross-1.jpg");
+        saveFlatCross(verticalObjectImage, verticalObject, 9, 18,"./src/main/resources/img/examples/vertical/vertical_polygonsFlat-20-42_cross-0.jpg");
+
+        verticalObjectImage = Draw.contour(verticalObjectImage, verticalObject, new Scalar(255,0,77), 5);
+        verticalObjectImage = Draw.polygonsFlat(verticalObjectImage, verticalObject, 2, new Scalar(255,255,255), 5, true);
+
+        saveImage(verticalObjectImage, "./src/main/resources/img/examples/vertical/vertical_central-lines_1-incorrect.jpg");
+        verticalObjectImage = verticalObjectImageSource.clone();
+
+        // Vertical correct
+        saveCross(verticalObjectImage, verticalObject, 21, 10,"./src/main/resources/img/examples/vertical/vertical_polygons-20-42_cross-0.jpg");
+
+        verticalObjectImage = Draw.contour(verticalObjectImage, verticalObject, new Scalar(255,0,77), 5);
+        verticalObjectImage = Draw.polygons(verticalObjectImage, verticalObject, 2, new Scalar(255,255,255), 5, true);
+
+        saveImage(verticalObjectImage, "./src/main/resources/img/examples/vertical/vertical_central-lines_2-correct.jpg");
+        verticalObjectImage = verticalObjectImageSource.clone();
+
+
+
+        // Horizontal flat
+        saveFlatCross(horizontalObjectImage, horizontalObject, 10, 21,"./src/main/resources/img/examples/horizontal/horizontal_polygonsFlat-20-42_cross-3.jpg");
+        saveFlatCross(horizontalObjectImage, horizontalObject, 10, 22,"./src/main/resources/img/examples/horizontal/horizontal_polygonsFlat-20-42_cross-2.jpg");
+        saveFlatCross(horizontalObjectImage, horizontalObject, 11, 23,"./src/main/resources/img/examples/horizontal/horizontal_polygonsFlat-20-42_cross-1.jpg");
+        saveFlatCross(horizontalObjectImage, horizontalObject, 11, 24,"./src/main/resources/img/examples/horizontal/horizontal_polygonsFlat-20-42_cross-0.jpg");
+
+        horizontalObjectImage = Draw.contour(horizontalObjectImage, horizontalObject, new Scalar(255,0,77), 5);
+        horizontalObjectImage = Draw.polygonsFlat(horizontalObjectImage, horizontalObject, 2, new Scalar(255,255,255), 5, true);
+
+        saveImage(horizontalObjectImage, "./src/main/resources/img/examples/horizontal/horizontal_central-lines_1-incorrect.jpg");
+        horizontalObjectImage = horizontalObjectImageSource.clone();
+
+        // Horizontal correct
+        saveCross(verticalObjectImage, verticalObject, 21, 10,"./src/main/resources/img/examples/vertical/vertical_polygons-20-42_cross-0.jpg");
+
+        horizontalObjectImage = Draw.contour(horizontalObjectImage, horizontalObject, new Scalar(255,0,77), 5);
+        horizontalObjectImage = Draw.polygons(horizontalObjectImage, horizontalObject, 2, new Scalar(255,255,255), 5, true);
+
+        saveImage(horizontalObjectImage, "./src/main/resources/img/examples/horizontal/horizontal_central-lines_2-correct.jpg");
+        horizontalObjectImage = horizontalObjectImageSource.clone();
     }
 }
